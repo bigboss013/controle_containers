e674b242017d37a9ffeda3032acf6c5d53faa8d1
BigBoss <bigbossgarage013@icloud.com>
2026-06-06 14:34:44 -0300

    fix: remove placeholder appId - Firestore works without it

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,9 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCje8V9yQcgJ6LJL1AhyViKq8ArtjARsRA",
   authDomain: "santos-transportes.firebaseapp.com",
   projectId: "santos-transportes",
-  storageBucket: "santos-transportes.firebasestorage.app",
-  messagingSenderId: "346700863655",
-  appId: "1:346700863655:web:placeholder"
+  storageBucket: "santos-transportes.firebasestorage.app"
 };
 
 firebase.initializeApp(firebaseConfig);
